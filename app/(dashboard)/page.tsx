3642c056a4559e691da160b7f49c4dcd0b1521f1
Marszell <josefmarcelinos@gmail.com>
2024-09-17 22:36:04 +0700

    add box

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import styles from "../ui/dashboard/dashboard.module.css";
+import Box from "../component/box/box";
 
 export default function Dashboard() {
   return (
     <div className={styles.page}>
         <span>content</span>
+        <div>
+            <Box />
+        </div>
     </div>
   );
 }
---
+++ app/component/box/box.tsx
@@ -0,0 +1,21 @@
+import styles from "./box.module.css"
+
+export default function Box () {
+    return(
+        <div className={styles.wrapper}>
+            <div className={styles.content}>
+                <span>content</span>
+            </div>
+            <div className={styles.description}>
+                <span>description</span>
+            </div>
+            <div className={styles.date}>
+                <span>13/11/2024</span>
+            </div>
+            <div className={styles.buttons}>
+                <button className={`${styles.button} ${styles.view}`}>View</button>
+                <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+            </div>
+        </div>
+    )
+}
