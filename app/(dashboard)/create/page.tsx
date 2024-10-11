3db0243865ccd9bb3f2bce9cb9d97f1d3a7b17f3
Marszell <josefmarcelinos@gmail.com>
2024-10-12 00:07:18 +0700

    add formik

--- app/(dashboard)/create/page.tsx
+++ app/(dashboard)/create/page.tsx
@@ -1,24 +1,55 @@
 "use client"
 
 import styles from "/app/ui/dashboard/create/create.module.css"
-import {Field, Formik} from "formik";
-import {useState} from "react";
+import { Field, Formik } from "formik";
+import { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import * as Yup from "yup";
 
 
-
-export default function Create() {
+export default function Create({isCreated,id}) {
     const[formData, setFormData] = useState({})
 
     const initialValues = {
         title: formData["title"] ?? "",
         description: formData["description"] ?? "",
     }
+
+    const onSubmit = (values) => {
+        try {
+            const formData = new FormData();
+            for (let value in values) {
+                formData.append(value, values[value]);
+            }
+            let response;
+            if (!isCreated) {
+                response = await axios.put(`/api/task/${id}`, formData)
+            }else{
+                response = await axios.post("/api/task/", formData)
+            }
+
+            if(response.status === 201 || response.status === 200) {
+                // navigate("/")
+                toast.success("Task successfully created")
+            }else {
+                toast.error("Error");
+            }
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+    }
+
+    const AddOrUpdateSchema = Yup.object().shape({
+        title: Yup.string().required("Please fill Tittle"),
+    })
+
     return (
         <Formik
             enableReinitialize={true}
             initialValues={initialValues}
-            // onSubmit={onSubmit}
-            // validationSchema={}
+            onSubmit={onSubmit}
+            validationSchema={AddOrUpdateSchema}
         >
             <div className={styles.container}>
                 <form action="" className={styles.form}>
