0debdf42c9b0ab7af2db9f1e863ac60c0d7504ad
Marszell <josefmarcelinos@gmail.com>
2024-10-16 22:10:06 +0700

    create single update

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,12 @@
 import {ReactNode} from "react";
 import styles from "../ui/dashboard/dashboard.module.css"
 import Sidebar from "../component/sidebar/sidebar";
+import {Toaster} from "react-hot-toast";
 
 export default function Layout ({children}: {children: ReactNode}) {
     return(
         <div className={styles.container}>
+            <Toaster position={"bottom-left"}/>
             <div className={styles.menu}>
                 <Sidebar />
             </div>
