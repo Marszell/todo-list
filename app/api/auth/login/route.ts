f11a0b573302d831aa489921ed633da657d8690e
Marszell <josefmarcelinos@gmail.com>
2024-11-19 22:50:08 +0700

    progress

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,8 +1,19 @@
-import {signIn} from '@/auth'
+import {signIn} from '@/auth';
+import {NextResponse} from "next/server";
+import {AuthError} from "next-auth";
+
 export async function POST(request: Request): Promise<void> {
     try {
         const formData = await request.formData();
         await signIn('credentials', formData);
-
+        return NextResponse.json({message: "Success", data:{}, error:{} }, {status:200});
+    }catch (error) {
+        if (error instanceof AuthError) {
+            return NextResponse.json({message:"Invalid Credentials", data:{}, error:error}, {status:400});
+        }
+        if(error.message === "NEXT_REDIRECT"){
+            return NextResponse.json({message:"Success", data:{}, error:{} }, {status:200});
+        }
+        return NextResponse.json({message:"", data:{}, error:error}, {status:500})
     }
 }
