a2d6f72e64752a5a3d57a645f32289cedb33a50d
Marszell <josefmarcelinos@gmail.com>
2024-12-09 23:35:22 +0700

    progress

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,9 @@
 "use client"
 import styles from './login.module.css'
-import toast, {Toaster} from "react-hot-toast";
+import {toast , Toaster} from "react-hot-toast";
 import axios from "axios";
 // import {router} from "next/client";
-import {useRouter} from "next/navigation";
+import {redirect, useRouter} from "next/navigation";
 import Link from "next/link";
 
 export default function LoginPage(){
@@ -11,15 +11,17 @@ export default function LoginPage(){
 
     const login = async (values) => {
         try {
-            const response = await axios.post("/login", values);
-            // console.log(response);
+            const response = await axios.post("/api/auth/login", values);
+            console.log(response);
             if (response.status === 201 || response.status === 200) {
+                console.log('test');
                 router.push("/");
             }
         }catch(error){
             toast.error(error.response.data.message);
         }
     }
+
     return (
         <div className={styles.container}>
             <Toaster position={"bottom-left"}/>
@@ -39,9 +41,9 @@ export default function LoginPage(){
                     placeholder={"Password"}
                     required
                 />
-                <Link href={"/signup"}>
-                    <button>Sign Up</button>
-                </Link>
+                {/*<Link href={"/signup"}>*/}
+                {/*    <button>Sign Up</button>*/}
+                {/*</Link>*/}
                 <button>Login</button>
             </form>
         </div>
