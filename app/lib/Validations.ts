069c8a9bedac6e31f9b31c681598e7d47cb4f287
Marszell <josefmarcelinos@gmail.com>
2024-12-13 20:02:35 +0700

    signup progress

--- app/lib/Validations.ts
+++ app/lib/Validations.ts
@@ -9,3 +9,28 @@ export const TodoFormSchema = z.object({
         required_error:"please input title of todo",
     }),
 });
+
+export const SignUpSchema = z.object({
+    name: z
+        .string ()
+        .min(2,{message: 'Name must be at least 2 characters long.'})
+        .trim(),
+    email: z
+        .string()
+        .email({message: 'Please enter valid email address.'})
+        .trim(),
+    password: z
+        .string()
+        .trim(),
+});
+
+export type FormState =
+    | {
+    errors?: {
+        name?: string[]
+        email?: string[]
+        password?: string[]
+    }
+    message?: string
+}
+    | undefined
